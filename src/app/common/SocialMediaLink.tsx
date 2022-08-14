cb2310e351aa78fe1a06c5417703be3615adc755
Nirav Chavda <nirav.c@incubyte.co>
2022-08-14 16:54:54 +0530

    FIX: Fix ES Lint Security Issue

--- src/app/common/SocialMediaLink.tsx
+++ src/app/common/SocialMediaLink.tsx
@@ -4,7 +4,7 @@ import React from "react";
 type SocialMediaLinkProps = { platform: string, children: React.ReactNode }
 
 const SocialMediaLink = ({platform, children}: SocialMediaLinkProps) => {
-  return <a href={getLink(platform)} target={"_blank"} className={'link-item'}>
+  return <a href={getLink(platform)} target={"_blank"} rel="noreferrer" className={'link-item'}>
     {children}
   </a>;
 }
