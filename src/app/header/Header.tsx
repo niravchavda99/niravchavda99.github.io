6b31c6e6329b7b64be011f1b88e10f924229827d
Nirav Chavda <nirav.c@incubyte.co>
2022-08-15 22:33:23 +0530

    ENH: Remove Links header

--- src/app/header/Header.tsx
+++ src/app/header/Header.tsx
@@ -16,7 +16,6 @@ const Header = ({darkMode, toggleDarkMode}: HeaderProps) => {
         <a href={"#about"} className={'header-content'}>About</a>
         <a href={"#experience"} className={'header-content'}>Experience</a>
         <a href={"#skills"} className={'header-content'}>Skills</a>
-        <a href={"#links"} className={'header-content'}>Links</a>
         <DarkModeToggle darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
       </div>
     </nav>
