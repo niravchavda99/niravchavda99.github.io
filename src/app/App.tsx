5a0e8f1036c6480b727f6f33bcb04061b6003e43
Nirav Chavda <nirav.c@incubyte.co>
2022-08-16 17:00:59 +0530

    FIX: Fix dark mode icon issue

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.scss';
 import Header from './header/Header';
 import ScrollToTop from 'react-scroll-to-top';
 import Footer from "./footer/Footer";
 import {Outlet} from "react-router-dom";
-import {getFromStorage} from "./Utils";
 
-function App() {
-  const mode = getFromStorage();
-  const [darkMode, setDarkMode] = useState(mode);
+type AppProps = {
+  darkMode: boolean,
+  toggleDarkMode: Function
+}
 
+function App({darkMode, toggleDarkMode}: AppProps) {
   return (
       <div className={`App ${darkMode ? 'dark' : 'light'}`}>
         <ScrollToTop className={'flex flex-col items-center justify-center'} smooth
                      color="#8b5cf6"/>
-        <Header darkMode={darkMode} toggleDarkMode={setDarkMode}/>
+        <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
         <Outlet/>
         <Footer/>
       </div>
--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -2,12 +2,17 @@ import {Navigate, Route, Routes} from "react-router-dom";
 import Projects from "./projects/Projects";
 import App from "./App";
 import Home from "./home/Home";
+import {getFromStorage} from "./Utils";
+import {useState} from "react";
 
 const AppRoutes = () => {
+  const mode = getFromStorage();
+  const [darkMode, setDarkMode] = useState(mode);
+
   return <Routes>
-    <Route path={"/"} element={<App/>}>
+    <Route path={"/"} element={<App darkMode={darkMode} toggleDarkMode={setDarkMode}/>}>
       <Route path={"projects"} element={<Projects/>}/>
-      <Route index element={<Home darkMode={false}/>}/>
+      <Route index element={<Home darkMode={darkMode}/>}/>
     </Route>
     <Route path="*" element={<Navigate to="/"/>}/>
   </Routes>;
