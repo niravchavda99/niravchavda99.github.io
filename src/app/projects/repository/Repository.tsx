49b2bf3f2951956991f66bb21ddc70ff19017bd1
Nirav Chavda <nirav.c@incubyte.co>
2022-08-21 11:54:34 +0530

    ENH: Update projects view

--- src/app/projects/repository/Repository.tsx
+++ src/app/projects/repository/Repository.tsx
@@ -1,8 +1,8 @@
 import './Repository.scss';
 import RepositoryDto from "../RepositoryDto";
-import {FiGithub} from "react-icons/fi";
 import {getLanguageIcon} from "../../Utils";
 import ThemeValueProps from "../../common/ThemeValueProps";
+import {FiGithub} from "react-icons/fi";
 
 type RepositoryProps = ThemeValueProps & { repository: RepositoryDto }
 
@@ -11,21 +11,25 @@ const Repository = ({repository, theme}: RepositoryProps) => {
   const icon = getLanguageIcon(language, theme);
 
   return (
-      <div className={'repository-container'}>
-        <div className={'h-full flex flex-col justify-between'}>
-          <div>
-            <div className="repository-title">{repository.name}</div>
-            <div className="repository-description">{repository.description}</div>
+      <div className={'repository-wrapper'}>
+        <div className="repository-container">
+          <div className={'h-full flex flex-col justify-between items-center'}>
+            <div>
+              <div className="repository-name">{repository.name}</div>
+              <p className="repository-description">{repository.description}</p>
+              <p className="repository-description">
+                {language &&
+                    <div className="repository-language">
+                      {icon} {language}
+                    </div>}
+              </p>
+            </div>
+            <a href={repository.htmlUrl} className="code-link" rel={"noreferrer"} target={"_blank"}>
+              <FiGithub className={'inline-block'} size={16}/>
+              <span className={'ml-1'}>Code</span>
+            </a>
           </div>
-          {language &&
-              <div className="repository-language">
-                {icon} {language}
-              </div>}
         </div>
-        <a className={"code-link"} href={repository.htmlUrl} rel={"noreferrer"} target={"_blank"}>
-          <FiGithub className={'inline-block'} size={16}/>
-          <span> Code</span>
-        </a>
       </div>
   );
 };
