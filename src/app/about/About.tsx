fb1ae61af34bff957e459dd4eaacef6ad9bfea65
Nirav Chavda <nmchavda99@gmail.com>
2023-09-16 11:20:51 +0530

    CLN: Remove unused imports

--- src/app/about/About.tsx
+++ src/app/about/About.tsx
@@ -2,9 +2,7 @@ import profileImage from '../assets/images/profile.jpg';
 import "./About.scss";
 import {TbMail} from "react-icons/tb";
 import {BsGithub, BsInstagram, BsLinkedin, BsTwitter} from "react-icons/bs";
-import {FaBlogger} from "react-icons/fa";
 import SocialMediaLink from "../common/SocialMediaLink";
-import {Link} from "react-router-dom";
 
 const About = () => {
   const iconSize = 28;
@@ -20,11 +18,11 @@ const About = () => {
     <div className={'about-name'}>Nirav Chavda</div>
     <div className={'about-short'}>I'm a Software Craftsperson, Learner and an Enthusiast</div>
     <div className={'links'}>
-      <SocialMediaLink platform={'mail'}><TbMail size={iconSize+4}/></SocialMediaLink>
+      <SocialMediaLink platform={'mail'}><TbMail size={iconSize + 4}/></SocialMediaLink>
       <SocialMediaLink platform={'github'}><BsGithub size={iconSize}/></SocialMediaLink>
-      <SocialMediaLink platform={'linkedin'}><BsLinkedin size={iconSize-2}/></SocialMediaLink>
+      <SocialMediaLink platform={'linkedin'}><BsLinkedin size={iconSize - 2}/></SocialMediaLink>
       <SocialMediaLink platform={'twitter'}><BsTwitter size={iconSize}/></SocialMediaLink>
-      <SocialMediaLink platform={'instagram'}><BsInstagram size={iconSize-2}/></SocialMediaLink>
+      <SocialMediaLink platform={'instagram'}><BsInstagram size={iconSize - 2}/></SocialMediaLink>
     </div>
   </div>;
 }
