f4804e07cc3539ed23474f856ce4bf2062c24b8f
Nirav Chavda <nmchavda99@gmail.com>
2023-09-16 11:06:13 +0530

    ENH: Move sticky header content to fixed header

--- src/app/header/Header.tsx
+++ src/app/header/Header.tsx
@@ -1,23 +1,33 @@
 import './Header.scss';
 import DarkModeToggle from "../darkmodetoggle/DarkModeToggle";
-import {ReactNode} from "react";
+import React, {ReactNode} from "react";
 import {Link} from "react-router-dom";
 import ThemeProps from "../common/ThemeProps";
 
 type HeaderProps = ThemeProps & { children?: ReactNode };
 
 const Header = ({theme, toggleTheme, children}: HeaderProps) => {
-  return <div>
-    <nav className={`header-container`}>
-      <div>
-        <Link to={"/"} className={'header-logo'}>Nirav Chavda</Link>
-      </div>
-      <div className={'header-content-container'}>
-        {children}
-        <DarkModeToggle theme={theme} toggleTheme={toggleTheme}/>
-      </div>
-    </nav>
-  </div>;
+  return <nav className={`header-container`}>
+    <div>
+      <Link to={"/"} className={'header-logo'}>Nirav Chavda</Link>
+    </div>
+    <div className={'header-content-container'}>
+      <a href={"/#about"} className={"header-content"}>
+        About
+      </a>
+      <a href={"/#experience"} className={"header-content"}>
+        Experience
+      </a>
+      <a href={"/#skills"} className={"header-content"}>
+        Skills
+      </a>
+      <a href={"/#activities"} className={"header-content"}>
+        Activities
+      </a>
+      <Link to={"/projects"} className={'header-content'}>Projects</Link>
+      <DarkModeToggle theme={theme} toggleTheme={toggleTheme}/>
+    </div>
+  </nav>;
 }
 
 export default Header;
--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -8,28 +8,23 @@ import SplashScreen from "../common/splashscreen/SplashScreen";
 import Fader from "../common/fader/Fader";
 import Activities from "../activities/Activities";
 import '../common/Common.scss';
-import {Link} from "react-router-dom";
-import StickyHeader from "../sticky-header/StickyHeader";
 
 const Home = ({theme, toggleTheme}: ThemeProps) => {
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        setTimeout(() => setLoading(false), 2100);
-    }, []);
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 2100);
+  }, []);
 
-    return loading ?
-        <SplashScreen/> :
-        <Fader timeout={200} transitionTime={1}>
-            <Header theme={theme} toggleTheme={toggleTheme}>
-                <Link to={"/projects"} className={'header-content'}>Projects</Link>
-            </Header>
-            <StickyHeader/>
-            <About/>
-            <Experience/>
-            <Skills theme={theme}/>
-            <Activities theme={theme}/>
-        </Fader>
+  return loading ?
+      <SplashScreen/> :
+      <Fader timeout={200} transitionTime={1}>
+        <Header theme={theme} toggleTheme={toggleTheme}/>
+        <About/>
+        <Experience/>
+        <Skills theme={theme}/>
+        <Activities theme={theme}/>
+      </Fader>
 };
 
 export default Home;
