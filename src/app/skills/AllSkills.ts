9f5f66f482bd5e423990844dae739cb6d03ef28b
Nirav Chavda <nmchavda99@gmail.com>
2023-09-23 12:49:21 +0530

    ENH: Reorder Skills

--- src/app/skills/AllSkills.ts
+++ src/app/skills/AllSkills.ts
@@ -15,42 +15,36 @@ export const getAllSkills = (theme: string): Skill[] => {
   const isDarkTheme = theme === "dark";
 
   return [
-    {name: "HTML", className: getDeviconClassName("html5-plain")},
-    {name: "CSS", className: getDeviconClassName("css3-plain")},
-    {name: "Javascript", className: getDeviconClassName("javascript-plain")},
-    {name: "Typescript", className: getDeviconClassName("typescript-plain")},
-    {
-      name: "Markdown",
-      className: getDeviconClassName("markdown-original", isDarkTheme),
-    },
     {name: "Java", className: getDeviconClassName("java-plain")},
-    {name: "Spring", className: getDeviconClassName("spring-plain")},
     {
       name: "Micronaut",
       className: getDeviconClassName(""),
       svg: isDarkTheme ? micronautDark : micronautLight,
     },
+    {name: "Javascript", className: getDeviconClassName("javascript-plain")},
+    {name: "Typescript", className: getDeviconClassName("typescript-plain")},
+    {name: "Angular", className: getDeviconClassName("angularjs-plain")},
+    {name: "React", className: getDeviconClassName("react-original")},
+    {name: "HTML", className: getDeviconClassName("html5-plain")},
+    {name: "CSS", className: getDeviconClassName("css3-plain")},
+    {name: "Bootstrap", className: getDeviconClassName("bootstrap-plain")},
+    {name: "Tailwind", className: getDeviconClassName("tailwindcss-plain")},
+    {name: "MS SQL Server", className: getDeviconClassName("microsoftsqlserver-plain")},
+    {
+      name: "Markdown",
+      className: getDeviconClassName("markdown-original", isDarkTheme),
+    },
+    {name: "Spring", className: getDeviconClassName("spring-plain")},
     {name: "NodeJS", className: getDeviconClassName("nodejs-plain")},
     {
       name: "Express",
       className: getDeviconClassName("express-original", isDarkTheme),
     },
-    {name: "React", className: getDeviconClassName("react-original")},
-    {name: "Angular", className: getDeviconClassName("angularjs-plain")},
-    {name: "Bootstrap", className: getDeviconClassName("bootstrap-plain")},
-    {name: "Tailwind", className: getDeviconClassName("tailwindcss-plain")},
     {name: "MySQL", className: getDeviconClassName("mysql-plain")},
-    {name: "MongoDB", className: getDeviconClassName("mongodb-plain")},
     {name: "Dart", className: getDeviconClassName("dart-plain")},
     {name: "Flutter", className: getDeviconClassName("flutter-plain")},
-    {name: "D3 JS", className: getDeviconClassName("d3js-plain")},
     {name: "Git", className: getDeviconClassName("git-plain")},
-    {name: "Firebase", className: getDeviconClassName("firebase-plain")},
-    {name: "VSCode", className: getDeviconClassName("vscode-plain")},
     {name: "IntelliJ", className: getDeviconClassName("intellij-plain")},
-    {
-      name: "Android Studio",
-      className: getDeviconClassName("androidstudio-plain"),
-    },
+    {name: "VSCode", className: getDeviconClassName("vscode-plain")},
   ];
 };
