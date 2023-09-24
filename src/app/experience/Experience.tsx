fc6a25435ac290293b9682274420639160d90f5b
Nirav Chavda <nmchavda99@gmail.com>
2023-09-24 10:42:02 +0530

    ENH: Remove usage of array index in key

--- src/app/experience/Experience.tsx
+++ src/app/experience/Experience.tsx
@@ -2,13 +2,18 @@ import "./Experience.scss";
 import React from "react";
 import {jobs} from "./jobs";
 import Job from "./job/Job";
+import {EducationProfile, JobProfile} from "./job/JobProfile";
 
 const Experience = () => {
   return <div id={'experience'}>
     <div className="experience-header">Experience & Education</div>
     <div className={'flex flex-col justify-center items-center'}>
       <div className="jobs-container">
-        {jobs.map((job, index) => <Job key={`job${index}`} profile={job}/>)}
+        {jobs.map((job) => {
+          const designation = job.type == "job" ? (job as JobProfile).designation : (job as EducationProfile).degree;
+          const jobKey = `job-${job.organizationName}-${designation}`;
+          return <Job key={jobKey} profile={job}/>;
+        })}
       </div>
     </div>
   </div>;
