fa8ace187fef45f22aeea33b69c457d03c30d0ae
Nirav Chavda <nmchavda99@gmail.com>
2023-09-24 11:06:10 +0530

    ENH: Add Glassmorphic to Projects

--- src/app/projects/repository/Repository.tsx
+++ src/app/projects/repository/Repository.tsx
@@ -3,6 +3,7 @@ import RepositoryDto from "../RepositoryDto";
 import {getLanguageIcon} from "../../Utils";
 import {FiGithub} from "react-icons/fi";
 import Shiny from "../../common/shiny/Shiny";
+import {GlassMorphic} from "../../common/glassmorphic/GlassMorphic";
 
 type RepositoryProps = { repository: RepositoryDto }
 
@@ -12,24 +13,27 @@ const Repository = ({repository}: RepositoryProps) => {
 
   return (
       <div className={'repository-wrapper'}>
-        <Shiny color={"#7C3AED"} size={150} classNames="repository-container">
-          <div className={'h-full flex flex-col justify-between items-center'}>
-            <div>
-              <div className="repository-name">{repository.name}</div>
-              <p className="repository-description">{repository.description}</p>
-              <p className="repository-description">
-                {language &&
-                    <span className="repository-language">
+        <GlassMorphic fadeOpacity={20} classNames={'w-full h-full'}>
+          <Shiny color={"#7C3AED"} size={200} classNames="repository-container">
+            <div className={'h-full flex flex-col justify-between items-center'}>
+              <div>
+                <div className="repository-name">{repository.name}</div>
+                <p className="repository-description">{repository.description}</p>
+                <p className="repository-description">
+                  {language &&
+                      <span className="repository-language">
                       {icon} {language}
                     </span>}
-              </p>
+                </p>
+              </div>
+              <a href={repository.htmlUrl} className="code-link" rel={"noreferrer"}
+                 target={"_blank"}>
+                <FiGithub className={'inline-block'} size={16}/>
+                <span className={'ml-1'}>Code</span>
+              </a>
             </div>
-            <a href={repository.htmlUrl} className="code-link" rel={"noreferrer"} target={"_blank"}>
-              <FiGithub className={'inline-block'} size={16}/>
-              <span className={'ml-1'}>Code</span>
-            </a>
-          </div>
-        </Shiny>
+          </Shiny>
+        </GlassMorphic>
       </div>
   );
 };
