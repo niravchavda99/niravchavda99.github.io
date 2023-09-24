dd17a8601ab1af627f8bf7a5db9f8ee81f141d89
Nirav Chavda <nmchavda99@gmail.com>
2023-09-24 11:02:14 +0530

    ENH: Add Glassmorphic to Activities

--- src/app/activities/activity/Activity.tsx
+++ src/app/activities/activity/Activity.tsx
@@ -1,6 +1,7 @@
 import './Activity.scss';
 import ActivityDto from "./ActivityDto";
 import Shiny from "../../common/shiny/Shiny";
+import {GlassMorphic} from "../../common/glassmorphic/GlassMorphic";
 
 type ActivityProps = {
   activity: ActivityDto
@@ -10,12 +11,14 @@ const Activity = ({activity}: ActivityProps) => {
 
   return (
       <div className={'activity-wrapper'}>
-        <Shiny size={150} color={"#7C3AED"} classNames="activity-container">
-          <h5 className="activity-title">{activity.title}</h5>
-          <p className="activity-subtitle">{activity.subtitle}</p>
-          <br/>
-          <p className="activity-description">{activity.description}</p>
-        </Shiny>
+        <GlassMorphic fadeOpacity={50}>
+          <Shiny size={200} color={"#7C3AED"} classNames="activity-container">
+            <h5 className="activity-title">{activity.title}</h5>
+            <p className="activity-subtitle">{activity.subtitle}</p>
+            <br/>
+            <p className="activity-description">{activity.description}</p>
+          </Shiny>
+        </GlassMorphic>
       </div>
   );
 };
