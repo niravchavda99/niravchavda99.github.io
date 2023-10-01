bba94a56ea3e3460b571db85d0ae5d73b7ac3f97
Nirav Chavda <nmchavda99@gmail.com>
2023-10-01 16:40:28 +0530

    ENH: Update activity key to not use array index

--- src/app/activities/Activities.tsx
+++ src/app/activities/Activities.tsx
@@ -9,12 +9,14 @@ const Activities = () => {
 
     <div className="flex justify-center">
       <a href="https://app.daily.dev/niravchavda99"><img
-        src="https://api.daily.dev/devcards/1e425277bd434101a24d2e9fcb45e7fe.png?r=xof" width="400"
-        alt="Nirav Chavda's Dev Card"/></a>
+          src="https://api.daily.dev/devcards/1e425277bd434101a24d2e9fcb45e7fe.png?r=xof"
+          width="400"
+          alt="Nirav Chavda's Dev Card"/></a>
     </div>
 
     <div className="activities-container">
-      {jobs.map((activity, index) => <Activity activity={activity} key={`activity${index}`}/>
+      {jobs.map((activity, index) => <Activity activity={activity}
+                                               key={`activity-${activity.title}`}/>
       )}
     </div>
   </div>;
