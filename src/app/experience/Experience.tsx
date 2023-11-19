749431edb305c570c8ae81348f4e40bf0543cc3d
Nirav Chavda <nmchavda99@gmail.com>
2023-11-19 12:21:35 +0530

    ENH: Update experience and education

--- src/app/experience/Experience.tsx
+++ src/app/experience/Experience.tsx
@@ -1,22 +1,61 @@
 import "./Experience.scss";
 import React from "react";
-import {jobs} from "./jobs";
-import Job from "./job/Job";
-import {EducationProfile, JobProfile} from "./job/JobProfile";
+import {educations, jobs} from "./constants";
+import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import {Profile} from "./Profile";
 
 const Experience = () => {
-  return <div id={'experience'}>
-    <div className="experience-header">Experience & Education</div>
-    <div className={'flex flex-col justify-center items-center'}>
-      <div className="jobs-container">
-        {jobs.map((job) => {
-          const designation = job.type === "job" ? (job as JobProfile).designation : (job as EducationProfile).degree;
-          const jobKey = `job-${job.organizationName}-${designation}`;
-          return <Job key={jobKey} profile={job}/>;
-        })}
-      </div>
-    </div>
-  </div>;
+    return <div id={'experience'}>
+        <div className="text-5xl p-2 pb-8 transition-all text-slate-200">Experience</div>
+        <div className={'flex flex-col justify-center items-center'}>
+            <VerticalTimeline>
+                {jobs.map(job => renderTimelineElement(job, job.organizationName, job.designation))}
+            </VerticalTimeline>
+        </div>
+
+        <div className={'flex flex-col justify-center items-center mt-4'}>
+            <VerticalTimeline>
+                {educations.map(education => renderTimelineElement(education, education.instituteName, education.degree))}
+            </VerticalTimeline>
+        </div>
+    </div>;
+}
+
+const renderTimelineElement = (profile: Profile, name: string, heading1: string) => {
+    return (
+        <VerticalTimelineElement
+            key={name}
+            date={`${profile.dateStarted} - ${profile.dateEnded}`}
+            dateClassName={'text-white'}
+            icon={<div className='flex justify-center items-center w-full h-full'>
+                <img src={profile.icon} alt={name}
+                     className='w-[70%] h-[70%] object-contain rounded-full'/>
+            </div>}
+            contentStyle={{
+                borderBottom: '8px',
+                borderStyle: 'solid',
+                borderBottomColor: profile.iconBg,
+                boxShadow: 'none',
+            }}
+            iconStyle={{background: profile.iconBg}}
+        >
+            <div className={'text-left'}>
+                <h3 className='text-black text-xl font-poppins font-semibold'>
+                    {heading1}
+                </h3>
+                <p className='text-black-500 font-medium font-base' style={{margin: 0}}>
+                    {profile.location}
+                </p>
+            </div>
+            {profile.points.length > 0 && <ul className='mt-4 list-disc ml-5 space-y-2 text-left'>
+                {profile.points.map(point => (
+                    <li key={`experience-${point}`}
+                        className='text-black-500/50 font-normal pl-1 text-small'>{point}</li>
+                ))}
+            </ul>}
+        </VerticalTimelineElement>
+    );
 }
 
 export default Experience;
