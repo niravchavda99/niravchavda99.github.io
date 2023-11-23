4c117a21fb04a4eb06d6291692bebf5178ebf942
Nirav Chavda <nmchavda99@gmail.com>
2023-11-23 16:33:45 +0530

    ENH: Update header logo

--- src/app/header/Header.tsx
+++ src/app/header/Header.tsx
@@ -3,23 +3,25 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 const Header = () => {
-  return <nav className={`header-container`}>
-    <div>
-      <Link to={"/"} className={'header-logo'}>Nirav Chavda</Link>
-    </div>
-    <div className={'header-content-container'}>
-      <a href={"/#experience"} className={"header-content mobile-hidden"}>
-        Experience
-      </a>
-      <a href={"/#skills"} className={"header-content mobile-hidden"}>
-        Skills
-      </a>
-      <a href={"/#activities"} className={"header-content mobile-hidden"}>
-        Activities
-      </a>
-      <Link to={"/projects"} className={'header-content'}>Projects</Link>
-    </div>
-  </nav>;
+    return <nav className={`header-container`}>
+        <div>
+            <Link to={"/"}
+                  className={'cursor-pointer p-2 text-xl w-10 h-10 rounded-lg bg-tertiary items-center justify-center flex font-bold shadow-md'}>
+                <p className='blue-gradient_text'>NC</p></Link>
+        </div>
+        <div className={'header-content-container'}>
+            <a href={"/#experience"} className={"header-content mobile-hidden"}>
+                Experience
+            </a>
+            <a href={"/#skills"} className={"header-content mobile-hidden"}>
+                Skills
+            </a>
+            <a href={"/#activities"} className={"header-content mobile-hidden"}>
+                Activities
+            </a>
+            <Link to={"/projects"} className={'header-content'}>Projects</Link>
+        </div>
+    </nav>;
 }
 
 export default Header;
