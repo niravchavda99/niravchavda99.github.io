7d7f95583a817ac31540d2c21c0e478d8ab4a675
Nirav Chavda <nmchavda99@gmail.com>
2024-06-01 14:42:34 +0530

    CLN: Reformat code

--- src/app/hash-generator/generate-hash.ts
+++ src/app/hash-generator/generate-hash.ts
@@ -1,16 +1,19 @@
-import {sha1, sha256, sha384, sha512} from 'crypto-hash';
+import { sha1, sha256, sha384, sha512 } from "crypto-hash";
 
-export type HashAlgorithm = 'sha1' | 'sha256' | 'sha384' | 'sha512';
+export type HashAlgorithm = "sha1" | "sha256" | "sha384" | "sha512";
 
-export function generateHash(hashAlgorithm: HashAlgorithm, text: string | ArrayBuffer | SharedArrayBuffer): Promise<string> {
-    switch (hashAlgorithm) {
-        case 'sha1':
-            return sha1(text);
-        case "sha256":
-            return sha256(text);
-        case 'sha384':
-            return sha384(text);
-        case 'sha512':
-            return sha512(text);
-    }
+export function generateHash(
+  hashAlgorithm: HashAlgorithm,
+  text: string | ArrayBuffer | SharedArrayBuffer
+): Promise<string> {
+  switch (hashAlgorithm) {
+    case "sha1":
+      return sha1(text);
+    case "sha256":
+      return sha256(text);
+    case "sha384":
+      return sha384(text);
+    case "sha512":
+      return sha512(text);
+  }
 }
