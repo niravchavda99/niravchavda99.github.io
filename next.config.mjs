6a269ff858384546bb4049fdd60e274277503c29
Nirav Chavda <nmchavda99@gmail.com>
2024-06-16 10:13:43 +0530

    ENH: Try deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    basePath: '/niravchavda99.github.io',
     distDir: 'dist',
+    images: {
+        unoptimized: true,
+    },
 }
 
 export default nextConfig
