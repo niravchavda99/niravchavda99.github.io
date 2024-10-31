6d68eb13f60ca89cbe3aec500103b2b1d876fad9
Nirav Chavda <nmchavda99@gmail.com>
2024-10-31 14:07:19 +0530

    ENH: Add GenForge to list of projects

--- src/app/projects/constants.ts
+++ src/app/projects/constants.ts
@@ -1,6 +1,7 @@
 import portfolioThreeJsSnap from "@/src/assets/images/portfolio-three-js-snap.png";
-import threeDPortfolioSnap from "@/src/assets/images/3d-portfolio-snap.png";
+import _3DPortfolioSnap from "@/src/assets/images/3d-portfolio-snap.png";
 import pokedexSnap from "@/src/assets/images/pokedex-snap.png";
+import genForgeSnap from "@/src/assets/images/gen-forge-snap.png";
 
 export interface ProjectTag {
   name: string;
@@ -16,45 +17,66 @@ export interface Project {
   tags: ProjectTag[];
 }
 
+const genForgeProject: Project = {
+  codeLink: "https://github.com/niravchavda99/gen-forge",
+  description: "Utility app to generate useful values on the fly!",
+  image: genForgeSnap.src,
+  link: "https://niravchavda99.github.io/gen-forge",
+  name: "GenForge",
+  tags: [
+    { name: "nextjs", color: "orange-text-gradient" },
+    { name: "react", color: "green-text-gradient" },
+    { name: "shadcn", color: "pink-text-gradient" },
+    { name: "tailwind", color: "blue-text-gradient" },
+  ],
+};
+
+const pokedexProject: Project = {
+  description: "Project to understand Next.js",
+  image: pokedexSnap.src,
+  link: "https://pokedex-six-neon.vercel.app",
+  name: "Pokédex",
+  tags: [
+    { name: "next.js", color: "blue-text-gradient" },
+    { name: "react", color: "orange-text-gradient" },
+    { name: "shadcn", color: "green-text-gradient" },
+    { name: "tailwind", color: "pink-text-gradient" },
+  ],
+};
+
+const _3DPortfolioProject: Project = {
+  codeLink: "https://github.com/niravchavda99/3d-portfolio-demo",
+  description: "Learning project to work with 3D elements in Javascript",
+  image: _3DPortfolioSnap.src,
+  link: "https://3d-portfolio-demo-chi.vercel.app",
+  name: "3D Portfolio",
+  tags: [
+    { name: "react", color: "orange-text-gradient" },
+    { name: "vite", color: "green-text-gradient" },
+    { name: "threejs", color: "blue-text-gradient" },
+    { name: "animation", color: "pink-text-gradient" },
+    { name: "3d", color: "orange-text-gradient" },
+  ],
+};
+
+const portfolioThreeJsProject: Project = {
+  codeLink: "https://github.com/niravchavda99/portfolio-threejs-demo",
+  description: "Learning project to work with 3D elements in Javascript",
+  image: portfolioThreeJsSnap.src,
+  link: "https://portfolio-threejs-demo.vercel.app/",
+  name: "Portfolio ThreeJS",
+  tags: [
+    { name: "react", color: "orange-text-gradient" },
+    { name: "vite", color: "green-text-gradient" },
+    { name: "threejs", color: "blue-text-gradient" },
+    { name: "animation", color: "pink-text-gradient" },
+    { name: "3d", color: "orange-text-gradient" },
+  ],
+};
+
 export const projects: Project[] = [
-  {
-    description: "Project to understand Next.js",
-    image: pokedexSnap.src,
-    link: "https://pokedex-six-neon.vercel.app",
-    name: "Pokédex",
-    tags: [
-      { name: "next.js", color: "blue-text-gradient" },
-      { name: "react", color: "orange-text-gradient" },
-      { name: "shadcn", color: "green-text-gradient" },
-      { name: "tailwind", color: "pink-text-gradient" },
-    ],
-  },
-  {
-    codeLink: "https://github.com/niravchavda99/3d-portfolio-demo",
-    description: "Learning project to work with 3D elements in Javascript",
-    image: threeDPortfolioSnap.src,
-    link: "https://3d-portfolio-demo-chi.vercel.app",
-    name: "3D Portfolio",
-    tags: [
-      { name: "react", color: "orange-text-gradient" },
-      { name: "vite", color: "green-text-gradient" },
-      { name: "threejs", color: "blue-text-gradient" },
-      { name: "animation", color: "pink-text-gradient" },
-      { name: "3d", color: "orange-text-gradient" },
-    ],
-  },
-  {
-    codeLink: "https://github.com/niravchavda99/portfolio-threejs-demo",
-    description: "Learning project to work with 3D elements in Javascript",
-    image: portfolioThreeJsSnap.src,
-    link: "https://portfolio-threejs-demo.vercel.app/",
-    name: "Portfolio ThreeJS",
-    tags: [
-      { name: "react", color: "orange-text-gradient" },
-      { name: "vite", color: "green-text-gradient" },
-      { name: "threejs", color: "blue-text-gradient" },
-      { name: "animation", color: "pink-text-gradient" },
-      { name: "3d", color: "orange-text-gradient" },
-    ],
-  },
+  genForgeProject,
+  pokedexProject,
+  _3DPortfolioProject,
+  portfolioThreeJsProject,
 ];
